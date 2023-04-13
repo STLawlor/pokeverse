9ba5c476189a7edeff1b9559a544c70e75cd9d25
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-04-13 17:12:23 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [pokeData, setPokeData] = useState();
+  const [filteredPokeData, setFilteredPokeData] = useState();
+
+  async function getpokeData() {
+    try {
+      const res = await fetch(`${pokeApi}`);
+      const data = await res.json();
+      setPokeData(data.results);
+      setFilteredPokeData(data.results);
+    } catch (err) {
+      console.log("An error occured! ", err)
+    }
+  }
+
+  function handleChange(e) {
+    const value = e.target.value;
+    const regex = new RegExp(value, 'gi');
+    const filteredData = pokeData.filter((pokemon) => {
+      return pokemon.name.match(regex);
+    });
+
+    setFilteredPokeData(filteredData);
+  }
+
+  useEffect(() => {
+    getpokeData();
+  }, []);
 
   return (
     <div data-testid="app">
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <InputGroup className="mb-3" >
+        <InputGroup.Text id="basic-addon1" >Search</InputGroup.Text>
+        <Form.Control
+          aria-label="search"
+          aria-describedby="basic-addon1"
+          onChange={() => handleChange(event)}
+        />
+      </InputGroup>
+      {filteredPokeData && 
+        filteredPokeData.map((pokemon, idx) => {
+          return (
+            <PokemonCard
+              key = { idx }
+              name= { pokemon.name }
+              url = { pokemon.url }
+            />
+          )
+        })
+      }
+      {!filteredPokeData &&
+        <Alert>
+          Fetching Pokemon data...
+        </Alert>
+      }
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function PokemonCard({ url, name }) {
+  const [pokemon, setPokemon] = useState();
+  const [sprite, setSprite] = useState();
+  const [abilities, setAbilities] = useState([]);
+
+  async function getPokemon() {
+    try {
+      const res = await fetch(`${url}`);
+      const data = await res.json();
+      setPokemon(data);
+      setAbilities(data.abilities);
+      setSprite(data.sprites.front_default);
+    } catch (err) {
+      console.log("An error occured! ", err);
+    }
+  }
+
+  useEffect(() => {
+    getPokemon();
+  }, [name]);
 
   return (
-    <div>
-        pokemon card
-    </div>
+    <>
+      {pokemon && (
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={sprite} />
+          <Card.Body>
+            <Card.Title>
+              <h4 className="text-capitalize">{name}</h4>
+            </Card.Title>
+            <Card.Text as="div">
+              <ul>
+                {abilities.map((ability, i) => {
+                  return (
+                    <li key={i}>{ability.ability.name}</li>
+                  )
+                })}
+              </ul>
+            </Card.Text>
+            <Button variant="primary">View Pokemon</Button>
+          </Card.Body>
+        </Card>
+      )}
+    </>
   );
 }
 
